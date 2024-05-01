6fa9017e7e52f2c575e52712c51eebdc152802fe
奔狼\Administrator <123@>
2024-05-01 18:46:37 +0800

    添加百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,14 +1,15 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-
+import { head } from "./head.js";
 export default defineUserConfig({
   base: "/",
-
+  // extra tags in `<head>`
+  head: head,
   lang: 'zh-CN',
   title: "一山的学习笔记",
   description: "✨一山的网站✨",
   theme,
-  
+
   // Enable it with pwa
   // shouldPrefetch: false,
 });
